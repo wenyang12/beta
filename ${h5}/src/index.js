37f7d2fc645b3ebc48aca510e11f9d9f113ab897
罗瑛 <luoying@fxiaoke.com>
2016-09-09 11:33:37 +0800

    H5模板调整

--- ${h5}/src/index.js
+++ ${h5}/src/index.js
@@ -3,34 +3,39 @@ require('./index.less');
 
 // 初始化App的相关模块引入
 require('libs/fastclick-attach');
-require('libs/touchmove-preventdefault');
-require('libs/touch-scroll');
+// require('libs/touchmove-preventdefault');
+// require('libs/touch-scroll');
+require('libs/network');
 
 // 引入数据采集模块
 let collector = require('libs/collector');
 collector.config('${h5}');
+
+let $ = require('zepto');
+let device = require('libs/device');
+
+let platform = device.ios ? 'ios' : (device.android ? 'android' : 'pc');
+let html = document.getElementById('fxiaoke');
+$(html).addClass(platform).addClass(window.isApp ? 'app' : '');
 
 // App初始化准备就绪
 let ready = require('libs/appready');
 ready('${h5}', null, () => {
   let React = require('react');
   let ReactDOM = require('react-dom');
-  let Router = require('react-router').Router;
-  let Route = require('react-router').Route;
-  let IndexRoute = require('react-router').IndexRoute;
+  let ReactRouter = require('react-router');
+  let Router = ReactRouter.Router;
+  let Route = ReactRouter.Route;
+  let IndexRoute = ReactRouter.IndexRoute;
 
   const App = React.createClass({
-    render: function() {
-      return (
-        <section className="${h5}-app">
-          {this.props.children}
-        </section>
-      );
+    render() {
+      return this.props.children;
     }
   });
 
   ReactDOM.render(
-    <Router>
+    <Router history={ReactRouter.HashHisotry}>
       <Route path="/" component={App}>
         <IndexRoute component={} />
       </Route>
